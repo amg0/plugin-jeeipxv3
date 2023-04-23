3763f152f672e9dcd9001ae1419a44518d385f7d
Alexis Mermet <alexis.mermet@gmail.com>
2023-04-23 17:57:19 +0200

    bug fix

--- core/class/jeeipxv3.class.php
+++ core/class/jeeipxv3.class.php
@@ -391,7 +391,8 @@ server: 192.168.0.17 port:3480
     if (is_object($eqLogic)) {
       if ( $eqLogic->getConfiguration('anselect',0)  != $antype) {
         $eqLogic->setConfiguration('anselect',$antype);
-        log::add(JEEIPXV3, 'debug', __METHOD__ .sprintf(" setting antype of eq:%d to anselect=%d",$this->getChildID($child),$antype));
+        log::add(JEEIPXV3, 'debug', __METHOD__ .sprintf(" setting anselect of eq:%s to anselect=%d",$this->getChildID($child),$antype));
+        $eqLogic->save();
       }
       $eqLogic->checkAndUpdateCmd('status', $value);
     } else {
