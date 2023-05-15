60c769fb0e8402a1509c6886882d0933358021ca
Alexis Mermet <alexis.mermet@gmail.com>
2023-05-15 19:12:30 +0200

    set counter

--- core/class/jeeipxv3.class.php
+++ core/class/jeeipxv3.class.php
@@ -717,6 +717,9 @@ class jeeipxv3Cmd extends cmd {
 				$root->setCounter($child,0);
 				break;
 			case 'setcounter':
+				$val = $_options['slider'];
+				$child = $root->splitLogicalID($eqLogic->getLogicalId())[1];  // return child
+				$root->setCounter($child,val);
 				break;
 			case 'btn_on':
 			case 'btn_off':
