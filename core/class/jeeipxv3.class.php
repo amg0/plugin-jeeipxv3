9e01611c18cd9e28553ff914928dec1dcb7196bc
Alexis Mermet <alexis.mermet@gmail.com>
2023-07-06 22:50:55 +0200

    change generic type

--- core/class/jeeipxv3.class.php
+++ core/class/jeeipxv3.class.php
@@ -214,7 +214,7 @@ public static function deamon_changeAutoMode($mode) {
 				$cmdEtat = $this->createOrUpdateCommand( 'Etat', 'status', 'info', 'binary', 1, 'ENERGY_STATE' );
 				$this->createOrUpdateCommand( 'On', $type.'_on', 'action', 'other', 1, 'LIGHT_ON', (int) $cmdEtat->getId() );
 				$this->createOrUpdateCommand( 'Off', $type.'_off', 'action', 'other', 1, 'LIGHT_OFF', (int) $cmdEtat->getId() );
-				$this->createOrUpdateCommand( 'Commute', $type.'_com', 'action', 'other', 0, 'GENERIC_ACTION', (int) $cmdEtat->getId() );
+				$this->createOrUpdateCommand( 'Commute', $type.'_com', 'action', 'other', 0, 'LIGHT_TOGGLE', (int) $cmdEtat->getId() );
 				break;
 			}
 			case 'analog': { // Analog
